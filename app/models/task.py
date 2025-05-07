34946b9abd9f33dc18b1196bc8cab1ae7cb69bf7
Laura Castro <lauracastrotech@gmail.com>
2025-05-07 09:16:22 -0400

    adds migration folder

--- app/models/task.py
+++ app/models/task.py
@@ -1,5 +1,30 @@
 from sqlalchemy.orm import Mapped, mapped_column
 from ..db import db
+from datetime import datetime
+from typing import Optional
 
 class Task(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    title: Mapped[str]
+    description: Mapped[str]
+    completed_at: Mapped[Optional[bool]]
+    
+# # Helper functions
+#     # to_dict()
+#     def to_dict(self):
+#         return {
+#                 "id":self.id,
+#                 "title":self.title,
+#                 "description":self.description,
+#                 "completed_at": datetime if self.completed_at else None
+#             }
+
+#     # @classmethod
+#     # from_dict()
+#     @classmethod
+#     def from_dict(cls, task_data):
+#         return cls(
+#             title=task_data["title"],
+#             description=task_data["description"],
+#             completed_at=task_data["completed_at"]
+#         )
